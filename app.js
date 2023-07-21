f9d7de64153220f93a1396b13ed5ef54b6e2890a
David Munyiri <davidmumyiri2019@gmail.com>
2023-07-21 16:25:56 +0300

    encountered a bug

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ let rated=document.querySelector('.rated')
 let submit=document.querySelector('.submit')
 let thankyouBox=document.querySelector('.thankyouBox')
 let hidden =document.querySelector('.hidden')
+let btn2=document.querySelector('.btn2')
+let btn3=document.querySelector('.btn3')
+let btn4=document.querySelector('.btn4')
+let btn5=doument.querySelector('.btn5')
 
 
 
@@ -13,6 +17,21 @@ console.log('cliked');
 })
 
 btn.addEventListener('click',function(){
-    rated.textContent='1'
-    console.log('1')
+    rated.textContent='1' 
+})
+
+btn2.addEventListener('click',function(){
+    rated.textContent='2' 
+})
+
+btn3.addEventListener('click',function(){
+    rated.textContent='3' 
+})
+
+btn4.addEventListener('click',function(){
+    rated.textContent='4' 
+})
+
+btn5.addEventListener('click',function(){
+    rated.textContent='5' 
 })
