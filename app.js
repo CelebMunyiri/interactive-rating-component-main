711b839ff248f817fc9a363fa1118b6fe63404c3
David Munyiri <davidmumyiri2019@gmail.com>
2023-07-21 16:26:33 +0300

    solved the bug whuch was due to use of wrong syntax

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let hidden =document.querySelector('.hidden')
 let btn2=document.querySelector('.btn2')
 let btn3=document.querySelector('.btn3')
 let btn4=document.querySelector('.btn4')
-let btn5=doument.querySelector('.btn5')
+let btn5=document.querySelector('.btn5')
 
 
 
